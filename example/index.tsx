39a3469b68eaaf34d5127f3e05216b7e1895733c
Loc Vo <v.locvt3@vinbrain.net>
2021-06-01 09:57:55 +0700

    First commit

---
+++ example/index.tsx
@@ -0,0 +1,15 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Thing } from '../.';
+import '../dist/debug-flag.cjs.development.css';
+
+const App = () => {
+  return (
+    <div>
+      <Thing />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import './index.css'
+
+// Delete me
+export const Thing = () => {
+  return <div className="root">DEBUG</div>;
+};
